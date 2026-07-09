6de989a7d532fdff7cd192560146dd836538a1b3
ananthakrishnan <asananthakrishnan01@gmail.com>
2026-07-09 23:00:47 +0530

    feat: WIP - my current work before develop setup

--- fmcg-frontend/dev-dist/sw.js
+++ fmcg-frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f389b5da'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.0vp8ocbnkt"
+    "revision": "0.14reaoas5mk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
