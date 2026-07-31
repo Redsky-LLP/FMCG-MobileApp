4a130cfc1cb3ddddb4e4bd516b885084c1b65687
ananthakrishnan <asananthakrishnan01@gmail.com>
2026-08-01 02:20:41 +0530

    feat: IST/UTC, the reopen date-bump, then the leftover local data

--- fmcg-frontend/dev-dist/sw.js
+++ fmcg-frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f389b5da'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.icsol7sdi8o"
+    "revision": "0.cinkqre43i4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
