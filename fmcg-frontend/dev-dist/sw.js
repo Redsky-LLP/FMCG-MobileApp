8ef9df3b6a3ad525afd56bc30358fff8ab5f0e8f
ananthakrishnan <asananthakrishnan01@gmail.com>
2026-09-03 23:29:49 +0530

    fix:  retail sheet gap

--- fmcg-frontend/dev-dist/sw.js
+++ fmcg-frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-f389b5da'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.t7u00udesf4"
+    "revision": "0.paq3stiah0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
